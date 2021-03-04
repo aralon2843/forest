e5c3c2d4711ca20da3ef91477f7305465ea6602f
aralon2843 <aralon2843@gmail.com>
2021-03-04 18:42:27 +0300

    made auto-recognition of the city

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let promise = new Promise((resolve) => {
   navigator.geolocation.getCurrentPosition((pos) => {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=ea516a7f9e0e55490e7b63ea06b65f54`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=ea516a7f9e0e55490e7b63ea06b65f54`;
     resolve(url);
   });
 }).then((url) => {
