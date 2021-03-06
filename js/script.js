c7b704bf39c89f18b9f3b7698d81d4d2dd2c781e
aralon2843 <aralon2843@gmail.com>
2021-03-06 23:10:56 +0300

    redesign, added functional for IP determination, need to fix browser determination

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let getPositionByBrowser = new Promise((resolve) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      getForecastData(data)
       renderCurrentWeather(data);
     });
 });
@@ -26,10 +27,11 @@ async function getPositionByIP() {
         .then((data) => resolve(data));
     }
   })
-    .then(
-      (data) =>
-        `https://api.openweathermap.org/data/2.5/weather?lat=${data.geoObjects.position[0]}&lon=${data.geoObjects.position[1]}&units=metric&appid=ea516a7f9e0e55490e7b63ea06b65f54`
-    )
+    .then((data) => {
+      getForecastData(data);
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.geoObjects.position[0]}&lon=${data.geoObjects.position[1]}&units=metric&appid=ea516a7f9e0e55490e7b63ea06b65f54`;
+      return url;
+    })
     .then((url) => {
       fetch(url)
         .then((response) => response.json())
@@ -39,28 +41,85 @@ async function getPositionByIP() {
     });
 }
 
-function renderCurrentWeather(data) {
-  console.log(data);
-  document.querySelector(".weather__info-city").innerHTML = data.name;
-  document.querySelector(
-    ".weather__info-date"
-  ).innerHTML = `${getCurrentMonth()} ${new Date().getDate()}, ${new Date().getFullYear()}`;
-  document.querySelector(
-    ".weather__info-img"
-  ).style.backgroundImage = `url('http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png'`;
-  document.querySelector(".weather__info-descr").innerHTML =
-    data.weather[0].description;
-  document.querySelector(".weather__deg-current").innerHTML = `${parseInt(
-    data.main.temp
-  )}°`;
-  document.querySelector(".weather__deg-max").innerHTML = `${parseInt(
-    data.main.temp_max
-  )}°<span>/</span>`;
-  document.querySelector(".weather__deg-min").innerHTML = `${parseInt(
-    data.main.temp_min
-  )}°`;
+async function getForecastData(data) {
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.geoObjects.position[0]}&lon=${data.geoObjects.position[1]}&units=metric&exclude=current,hourly,minutely,alerts&appid=ea516a7f9e0e55490e7b63ea06b65f54`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((forecastData) => {
+      console.log(forecastData);
+      document.querySelectorAll(".weather__item").forEach((item, i) => {
+        item.querySelectorAll(".weather__item-day").forEach((item) => {
+          item.innerHTML = getCurrentDay(new Date().getDay() + 1 + i)
+            .substring(0, 3)
+            .toUpperCase();
+        });
+        item.querySelectorAll(".weather__item-image").forEach((item) => {
+          item.setAttribute(
+            "src",
+            `http://openweathermap.org/img/wn/${forecastData.daily[1 + i].weather[0].icon}@4x.png`
+          );
+        });
+        item.querySelectorAll('.weather__item-deg').forEach(item => {
+          item.textContent = `${parseInt(forecastData.daily[1 + i].temp.min)}°/${parseInt(forecastData.daily[1 + i].temp.max)}°`
+        })
+      });
+    });
 }
 
+function renderCurrentWeather(data) {
+  document.querySelector(
+    ".info__date-time"
+  ).innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}`;
+  document.querySelector(".info__location-city").innerHTML = data.name;
+  document.querySelector(".info__location-country").innerHTML =
+    data.sys.country;
+  document.querySelector(".info__date-day").innerHTML = `${getCurrentDay(
+    new Date().getDay()
+  )}, ${new Date().getDate()} ${getCurrentMonth()} ${new Date().getFullYear()}`;
+  document
+    .querySelector(".weather__current-image")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`
+    );
+  document.querySelector(".weather__current-descr").innerHTML =
+    data.weather[0].description;
+  document.querySelector(".weather__current-deg").innerHTML = `${parseInt(
+    data.main.temp
+  )}°`;
+}
+function getCurrentDay(currentDay) {
+  switch (currentDay) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    case 8:
+      return "Monday";
+    case 9:
+      return "Tuesday";
+    case 10:
+      return "Wednesday";
+    case 11:
+      return "Thursday";
+    case 12:
+      return "Friday";
+    case 13:
+      return "Saturday";
+  }
+}
 function getCurrentMonth() {
   switch (new Date().getMonth()) {
     case 0:
